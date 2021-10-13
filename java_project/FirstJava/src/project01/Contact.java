ebe0b6271739c91881d7924530342f1c8ace328d
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-13 22:51:32 +0900

    Update Contact.java

--- java_project/FirstJava/src/project01/Contact.java
+++ java_project/FirstJava/src/project01/Contact.java
@@ -1,7 +1,5 @@
 package project01;
 
-import java.util.Scanner;
-
 public class Contact {
 
 	private String name;		// 이름
@@ -10,8 +8,6 @@ public class Contact {
 	private String address;		// 주소
 	private String birthDay;	// 생일
 	private String group;		// 그룹
-	
-	static Scanner scanner;		// 사용자의 입력을 받기 위한 scanner 클래스 변수 선언
 	
 	// getter 와 setter 메소드
 	public String getName() {
@@ -64,12 +60,7 @@ public class Contact {
 	
 	// 기본 생성자
 	public Contact() {
-		this.name = insertName();
-		this.phoneNumber = insertPhone();
-		this.eMail = insertEMail();
-		this.address = insertAddress();
-		this.birthDay = insertBirthDay();
-		this.group = insertGroup();
+		
 	}
 
 	// 매개변수 있는 생성자
@@ -82,135 +73,5 @@ public class Contact {
 		this.group = group;
 	}
 	
-	// 이름 입력 메소드
-	public String insertName() {
-		System.out.println("이름을 입력해주세요.");
-		System.out.print("> ");
-		String name = new String(scanner.nextLine());
-		return name;
-	}
-	
-	// 전화번호 입력 메소드
-	public String insertPhone() {
-		System.out.println("전화번호를 입력해주세요. (예. 010-0000-0000)");
-		System.out.print("> ");
-		String phoneNumber = new String(scanner.nextLine());
-		return phoneNumber;
-	}
-	
-	// 이메일 입력 메소드
-	public String insertEMail() {
-		System.out.println("이메일 주소를 입력해주세요. (예. email@email.com)");
-		System.out.print("> ");
-		String eMail = new String(scanner.nextLine());
-		return eMail;
-	}
-	
-	// 주소 입력 메소드
-	public String insertAddress() {
-		System.out.println("주소를 입력해주세요.");
-		System.out.print("> ");
-		String address = new String(scanner.nextLine());
-		return address;
-	}
-	
-	// 생일 입력 메소드
-	public String insertBirthDay() {
-		System.out.println("생일을 입력해주세요. (예. 1900-01-01)");
-		System.out.print("> ");
-		String birthDay = new String(scanner.nextLine());
-		return birthDay;
-	}
-	
-	// 그룹 입력 메소드
-	public String insertGroup() {
-		System.out.println("그룹을 입력해주세요.");
-		System.out.print("> ");
-		String group = new String(scanner.nextLine());
-		return group;
-	}
-	
-	// 정보 출력 메소드
-	public void printInfo() {
-		System.out.println("\n=====================================");
-		System.out.println("이름: " + getName());
-		System.out.println("전화번호: " + getPhoneNumber());
-		System.out.println("이메일주소: " + geteMail());
-		System.out.println("주소: " + getAddress());
-		System.out.println("생일: " + getBirthDay());
-		System.out.println("그룹: " + getGroup());
-		System.out.println("=====================================\n");
-	}
-	
-	// 정보 수정 메소드
-	public void updateInfo() {
-		System.out.println("정보를 수정합니다.\n");
-		System.out.println("수정할 이름을 입력해주세요.");
-		System.out.print("> ");
-		name = new String(scanner.nextLine());
-		
-		System.out.println("수정할 전화번호를 입력해주세요. (예. 010-0000-0000)");
-		System.out.print("> ");
-		phoneNumber = new String(scanner.nextLine());
-		
-		System.out.println("수정할 이메일 주소를 입력해주세요. (예. email@email.com)");
-		System.out.print("> ");
-		eMail = new String(scanner.nextLine());
-		
-		System.out.println("수정할 주소를 입력해주세요.");
-		System.out.print("> ");
-		address = new String(scanner.nextLine());
-		
-		System.out.println("수정할 생일을 입력해주세요. (예. 1900-01-01)");
-		System.out.print("> ");
-		birthDay = new String(scanner.nextLine());
-		
-		System.out.println("수정할 그룹을 입력해주세요.");
-		System.out.print("> ");
-		group = new String(scanner.nextLine());
-	}
-	
-	public static void main(String[] args) {
-		scanner = new Scanner(System.in);	// scanner 사용을 위해 초기화
-		
-		// 생성자 호출
-		Contact cont = new Contact();
-		
-		// 변수값 반환하는 getter 메소드를 호출하여 데이터 출력
-		System.out.println("이름: " + cont.getName());
-		System.out.println("전화번호: " + cont.getPhoneNumber());
-		System.out.println("이메일주소: " + cont.geteMail());
-		System.out.println("주소: " + cont.getAddress());
-		System.out.println("생일: " + cont.getBirthDay());
-		System.out.println("그룹: " + cont.getGroup());
-		
-		// 정보 출력 메소드 실행
-		cont.printInfo();
-		
-		// 변수의 값을 바꾸는 setter 메소드를 이용하여 데이터 수정
-		System.out.println("setter 메소드를 이용하여 데이터를 수정합니다.");
-		String temp = new String(cont.insertName());
-		cont.setName(temp);
-		temp = new String(cont.insertPhone());
-		cont.setPhoneNumber(temp);
-		temp = new String(cont.insertEMail());
-		cont.seteMail(temp);
-		temp = new String(cont.insertAddress());
-		cont.setAddress(temp);
-		temp = new String(cont.insertBirthDay());
-		cont.setBirthDay(temp);
-		temp = new String(cont.insertGroup());
-		cont.setGroup(temp);
-		
-		// 정보 출력 메소드 실행
-		cont.printInfo();
-		
-		// 정보 수정 메소드 실행
-		cont.updateInfo();
-		
-		// 정보 출력 메소드 실행
-		cont.printInfo();
-		
-		scanner.close();	// scanner 사용 종료
-	}
+
 }
