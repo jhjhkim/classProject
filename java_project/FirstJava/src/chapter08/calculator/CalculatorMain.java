3a5d669b47689b41cf3b6f33c25d57a304df0e2f
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 15:38:56 +0900

    Create CalculatorMain.java

---
+++ java_project/FirstJava/src/chapter08/calculator/CalculatorMain.java
@@ -0,0 +1,13 @@
+package chapter08.calculator;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		
+		CalculatorClass c = new CalculatorClass(3, 5);
+		
+		c.showResult();
+		
+	}
+	
+}
