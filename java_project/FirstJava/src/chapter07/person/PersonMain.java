e8390d2b80f6b7149813d22b4b2e5fdc000588ee
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 14:20:19 +0900

    Create PersonMain.java

---
+++ java_project/FirstJava/src/chapter07/person/PersonMain.java
@@ -0,0 +1,16 @@
+package chapter07.person;
+
+public class PersonMain {
+	
+	public static void main(String[] args) {
+		Person p1 = new Male("홍길동", "990101-1234567");
+		Person p2 = new Female("허초희", "011231-4567890");
+		Male m1 = new Male("김철수", "030505-3456789");
+		Female f1 = new Female("김영희", "881010-2345678");
+		
+		p1.hello();
+		p2.hello();
+		m1.hello();
+		f1.hello();
+	}
+}
