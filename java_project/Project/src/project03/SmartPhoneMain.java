6a07b4e8b9fd7d33de376da77e562d8a519a5e55
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 18:22:35 +0900

    Create SmartPhoneMain.java

---
+++ java_project/Project/src/project03/SmartPhoneMain.java
@@ -0,0 +1,24 @@
+package project03;
+
+public class SmartPhoneMain {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("=======================");
+		System.out.println("\t연락처 관리 프로그램");
+		System.out.println("=======================");
+		System.out.println(" 1. 회사 연락처 등록");
+		System.out.println(" 2. 거래처 연락처 등록");
+		System.out.println(" 3. 연락처 목록 보기");
+		System.out.println(" 4. 연락처 검색");
+		System.out.println(" 5. 연락처 삭제");
+		System.out.println(" 6. 연락처 수정");
+		System.out.println(" 7. 프로그램 종료");
+		System.out.println("=======================");
+		System.out.println("메뉴를 입력하세요. >>>");
+		
+		
+		
+	}
+
+}
