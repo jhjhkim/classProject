8ea81ec1e0d4eb4ac80dd9badb33eb28d2aaf89d
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 20:54:25 +0900

    Create CustomerContact.java

---
+++ java_project/Project/src/project04/CustomerContact.java
@@ -0,0 +1,66 @@
+package project04;
+
+// ② CustomerContact 회사, 거래처의 정보를 저장하는 하위 클래스를 정의합니다.
+//	- 거래처회사이름, 거래품목, 직급 변수 추가
+//	- 정보를 출력하는 메소드를 오버라이딩 해서 추가된 정보를 추가해서 출력
+public class CustomerContact extends Contact {
+
+	private String customerName;	// 거래처 이름
+	private String item;			// 거래품목
+	private String rank;			// 직급
+	
+	public CustomerContact() {
+		super();
+		this.customerName = "";
+		this.item = "";
+		this.rank = "";
+	}
+	
+	public CustomerContact(String name, String phoneNumber, String eMail, String address, String birthday, String group,
+						String customerName, String item, String rank) {
+		super(name, phoneNumber, eMail, address, birthday, group);
+		this.customerName = customerName;
+		this.item = item;
+		this.rank = rank;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+	
+	@Override
+	public void showData() {
+		super.showData();
+		System.out.println("거래처: " + this.customerName);
+		System.out.println("품목: " + this.item);
+		System.out.println("직급: " + this.rank);
+	}
+	
+	@Override
+	public void showBasicInfo() {
+		System.out.print("[" + this.customerName + "] ");
+		System.out.println("이름: " + super.getName());
+		System.out.println("전화번호: " + super.getPhoneNumber());
+	}
+	
+}
