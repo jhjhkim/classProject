010a7f7493ec5088ca83626c31100fbada0c215f
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 20:29:30 +0900

    Update CustomerContact.java

--- java_project/Project/src/project03/CustomerContact.java
+++ java_project/Project/src/project03/CustomerContact.java
@@ -1,5 +1,8 @@
 package project03;
 
+// ② CustomerContact 회사, 거래처의 정보를 저장하는 하위 클래스를 정의합니다.
+//	- 거래처회사이름, 거래품목, 직급 변수 추가
+//	- 정보를 출력하는 메소드를 오버라이딩 해서 추가된 정보를 추가해서 출력
 public class CustomerContact extends Contact {
 
 	private String customerName;	// 거래처 이름
