ba0a405cebea7996b610820c608d4d978b2e11d1
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 13:23:43 +0900

    Create Male.java

---
+++ java_project/FirstJava/src/exam/Male.java
@@ -0,0 +1,19 @@
+package exam;
+
+public class Male extends Person {
+
+	String name;
+	String idNumber;
+	String gender;
+	
+	public Male(String name, String idNumber) {
+		super(name, idNumber);
+		this.gender = "남자";
+	}
+
+	@Override
+	public void hello() {
+		super.hello();
+		System.out.println(this.gender + "입니다.");
+	}
+}
