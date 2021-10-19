56f062c5f14e63046ca2183bfa52746ede98b499
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-19 20:29:29 +0900

    Update CompanyContact.java

--- java_project/Project/src/project03/CompanyContact.java
+++ java_project/Project/src/project03/CompanyContact.java
@@ -1,5 +1,8 @@
 package project03;
 
+// ① CompanyContact 회사, 거래처의 정보를 저장하는 하위 클래스를 정의합니다.
+//	- 회사이름, 부서이름, 직급 변수 추가
+//	- 정보를 출력하는 메소드를 오버라이딩 해서 추가된 정보를 추가해서 출력
 public class CompanyContact extends Contact {
 
 	private String company;	// 회사이름
