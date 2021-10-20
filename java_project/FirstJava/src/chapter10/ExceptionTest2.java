f20b5a42147a3c0646dae25036d9a1b24da3acd0
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-20 18:21:07 +0900

    Create ExceptionTest2.java

---
+++ java_project/FirstJava/src/chapter10/ExceptionTest2.java
@@ -0,0 +1,57 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class ExceptionTest2 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[3];
+		Object o = new String("abc");
+		String str = null;
+		String number = "one";
+		
+		try {
+			// 배열 인덱스를 잘못 처리한 경우
+			System.out.println(arr[3]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("예외 발생!!!");
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			// 잘못된 형변환
+			Integer i = (Integer)o;
+		} catch (ClassCastException e) {
+			System.out.println("예외 발생!!!");
+			System.out.println(e.getMessage());			
+		}
+		
+		try {
+			// 잘못된 배열 크기
+			int[] arr1 = new int[-10];
+		} catch (NegativeArraySizeException e) {
+			System.out.println("예외 발생!!!");
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			System.out.println(str.toUpperCase());
+		} catch (NullPointerException e) {
+			System.out.println("예외 발생!!!");
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			// String -> int
+			int temp = Integer.parseInt(number);
+		} catch (NumberFormatException e) {
+			System.out.println("예외 발생!!!");
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("프로그램을 종료합니다.");
+		
+	}
+	
+}
