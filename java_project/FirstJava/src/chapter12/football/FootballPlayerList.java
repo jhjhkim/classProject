93b53d07b3a4f8d4bdcf8af279b792b3a4ebe0b5
jhjhkim <jihyun.kim.cd@gmail.com>
2021-10-26 22:50:05 +0900

    ch.12 - 2. 파일 입출력

--- java_project/FirstJava/src/chapter12/football/FootballPlayerList.java
+++ java_project/FirstJava/src/chapter12/football/FootballPlayerList.java
@@ -13,7 +13,7 @@ import java.util.List;
 //①축구선수 정보 인스턴스를 List<E>에 저장하는 프로그램을 만들어 봅시다.
 //②이 인스턴스 들을 파일로 저장하는 기능을 만들어 봅시다.
 //③저장된 파일을 객체로 만드는 기능을 만들어봅시다.
-public class PlayerList {
+public class FootballPlayerList {
 
 	ArrayList<FootballPlayer> list;
 	
