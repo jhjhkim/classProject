3140460636cfe33ce4adaa800a7685a8747bad58
jhjhkim <jihyun.kim.cd@gmail.com>
2022-01-03 16:55:06 +0900

    01.03. Java Config를 이용한 DI

--- spring_project/MemberManager/src/mm/service/MemberRegService2.java
+++ spring_project/MemberManager/src/mm/service/MemberRegService2.java
@@ -1,5 +1,7 @@
 package mm.service;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,31 +15,32 @@ import mm.exception.DuplicateMemberException;
 @Component("regService")
 public class MemberRegService2 {
 
-	@Autowired
+	//@Autowired
 	//@Qualifier("kkd")
-	private Dao guestDao;	
+	@Resource(name="guestDao")
+	private Dao dao;	
 	
 	// Dao 타입의 객체를 주입 받는 방법 : 생성자, setter 메소드
 	public MemberRegService2() {}
 	
 	public MemberRegService2(Dao dao) {
-		this.guestDao = dao;
+		this.dao = dao;
 	}
 	
 	public void setDao(Dao dao) {
-		this.guestDao = dao;
+		this.dao = dao;
 	}
 	
 	public void regMember(RegRequest request) throws DuplicateMemberException {
 		
 		// 이메일 중복 체크
-		Member member = guestDao.selectByEmail(request.getEmail());
+		Member member = dao.selectByEmail(request.getEmail());
 		
 		if(member != null) {
 			throw new DuplicateMemberException("이미 존재하는 이메일입니다.");
 		}
 		
-		guestDao.insert(request.toMember());
+		dao.insert(request.toMember());
 		System.out.println("[msg : 등록되었습니다.]");
 	}
 	
